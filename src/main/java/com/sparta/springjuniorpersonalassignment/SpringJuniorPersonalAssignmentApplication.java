aaf0297fc27debbd43a5e2ab2e31b884854bda32
Josh_corgi <whvkek@naver.com>
2024-08-16 01:22:46 +0900

    Init Project

---
+++ src/main/java/com/sparta/springjuniorpersonalassignment/SpringJuniorPersonalAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.springjuniorpersonalassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJuniorPersonalAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJuniorPersonalAssignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sparta/springjuniorpersonalassignment/SpringJuniorPersonalAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.springjuniorpersonalassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJuniorPersonalAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
